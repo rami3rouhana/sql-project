4bf7d9117829b2234bf6d894ee6e6c4821fe32b7
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-11 19:30:48 +0300

    refactor join to where

--- oscars.sql
+++ oscars.sql
@@ -1,4 +1,10 @@
-SELECT name, earnings_rank FROM movies 
-INNER JOIN oscars
-where oscars.type="best-picture"
-ORDER BY earnings_rank ASC LIMIT 1 ;
+SELECT name, earnings_rank FROM movie
+WHERE earnings_rank = (
+    SELECT MIN(earnings_rank)
+    FROM movie
+)
+AND id IN (
+	SELECT movie_id
+	FROM oscar
+	where type="best-picture"
+)
