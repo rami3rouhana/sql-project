bcbd276381ad7d593a44696883858e9128a3d83b
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-11 14:08:35 +0300

    refactor oscar query

--- oscars.sql
+++ oscars.sql
@@ -1,6 +1,4 @@
-(SELECT name, earnings_rank, name, earnings_rank FROM movie 
-ORDER BY earnings_rank DESC LIMIT 1 )
-UNION ALL
-(SELECT name, earnings_rank, name, earnings_rank FROM movie 
-ORDER BY earnings_rank ASC LIMIT 1 )
-
+(SELECT name, earnings_rank FROM movies 
+INNER JOIN oscars
+where oscars.type="best-picture"
+ORDER BY earnings_rank ASC LIMIT 1 );
