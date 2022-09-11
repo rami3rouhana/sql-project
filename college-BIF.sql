e61410071312a7ae38de8b53102548fdb7312e4f
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-11 19:17:34 +0300

    refactor bif

--- college-BIF.sql
+++ college-BIF.sql
@@ -1,7 +1,14 @@
 -- Find all courses taken by BIF majors
 SELECT courses.name FROM courses 
-LEFT JOIN enrolled ON enrolled.courses_crn = courses.crn
-LEFT JOIN students ON students.id = enrolled.students_id
-LEFT JOIN majors_in ON majors_in.students_id = enrolled.students_id
-LEFT JOIN departments ON departments.id = majors_in.departments_id
-WHERE departments.name="BIF"
+SELECT courses.name FROM courses 
+WHERE courses.crn IN (
+        SELECT courses_crn
+        FROM enrolled 
+		WHERE students_id IN (
+            SELECT students_id 
+            FROM majors_in
+            WHERE departments_id IN (
+                SELECT MAX(id)
+                FROM departments 
+                WHERE name = "BIF"
+    )))
