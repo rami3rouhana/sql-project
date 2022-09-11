3414c353629d6cd098088f2973a50f3f467254a9
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-11 21:37:46 +0300

    fix condition

--- college-courses.sql
+++ college-courses.sql
@@ -1,2 +1,6 @@
 -- Find the course or courses with the earliest start time
-SELECT * from courses where start_time= (SELECT Min(start_time) from courses)
+SELECT * from courses 
+where start_time IN (
+    SELECT Min(start_time) 
+    from courses
+    )
